d10e1bac27ccebca27f947c13b5f4cf4b7ede5e8
sennsy12 <122060247+sennsy12@users.noreply.github.com>
2023-01-11 14:30:56 +0100

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			component: Home
+		},
+		{
+			path: '/about',
+			component: () => import('../views/About.vue')
+		},
+		{
+			path: '/calendar',
+			component: () => import('../views/Calendar.vue')
+		},
+	],
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+	<main id="Home-page">
+		<h1>Home</h1>
+		<p>
+GAT-Ship is the largest independent software provider for ship management and operations. Our goal is for our customers to have the best and most comprehensive tools available to ensure their success.
+
+Our software gives you the the best opportunity to optimize and improve your business.</p>
+	</main>
+</template>
